a82f93eaee7b18f44aaa5c40ec42d526e53b6224
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2022-08-08 13:00:13 +0300

    task-8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const refs = {
+  form: document.querySelector('.login-form'),
+};
 
+refs.form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  const userData = {
+    [email.name]: email.value,
+    [password.name]: password.value,
+  };
+
+  console.log('userData: ', userData);
+
+  event.currentTarget.reset();
+}
