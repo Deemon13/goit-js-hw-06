b38a0ba47a6c68183a076d4217e9050d222f7dc0
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2022-08-08 13:55:39 +0300

    task-9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,21 @@
+const refs = {
+  body: document.querySelector('body'),
+  colorChanger: document.querySelector('.change-color'),
+  colorText: document.querySelector('.color'),
+};
+
+let currentColor = null;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function colorChangeHandler() {
+  currentColor = getRandomHexColor();
+
+  refs.colorText.textContent = currentColor;
+
+  refs.body.style.backgroundColor = currentColor;
+}
+
+refs.colorChanger.addEventListener('click', colorChangeHandler);
