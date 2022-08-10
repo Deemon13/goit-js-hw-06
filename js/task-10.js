740f0c5c2e4ab4d10dd8ed6ede47ca3e06ae6f6f
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2022-08-10 21:35:40 +0300

    task-10 done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
+const controls = document.querySelector('#controls');
+const divContainer = document.querySelector('#boxes');
+
+const userInput = controls.querySelector('input');
+const divCreator = controls.querySelector('button[data-create]');
+const divDestroyer = controls.querySelector('button[data-destroy]');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement('div');
+    div.style.width = `${i * 10 + 30}px`;
+    div.style.height = `${i * 10 + 30}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    divContainer.append(div);
+  }
+}
+
+function onClickCreateBoxes() {
+  destroyBoxes();
+
+  const numberOfDivs = Number(userInput.value);
+
+  createBoxes(numberOfDivs);
+}
+
+function destroyBoxes() {
+  divContainer.innerHTML = '';
+}
+
+divCreator.addEventListener('click', onClickCreateBoxes);
+divDestroyer.addEventListener('click', destroyBoxes());
